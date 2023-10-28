a87bea688aec492aff155a6d3bf6790ffcac61f3
sourabpanch7 <panchanansourav@gmail.com>
2023-10-28 13:12:28 +0530

    Added unit test cases for pyspark 3.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.1.0",
     author="sourabpanch7",
     description="Sample pyspark based ETL framework",
-    packages=setuptools.find_packages(),
-    python_requires = "==3.9",
+    packages=["src"],
+    python_requires="==3.9",
     zip_safe=False
 )
